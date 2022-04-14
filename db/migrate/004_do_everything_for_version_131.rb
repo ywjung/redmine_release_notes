23a98d3c2ee670743db28f7f7fc6972ed00862c7
Yongwook Jung <ywjung99@naver.com>
2022-04-14 15:34:44 +0900

    Update 004_do_everything_for_version_131.rb

--- db/migrate/004_do_everything_for_version_131.rb
+++ db/migrate/004_do_everything_for_version_131.rb
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License along with
 # redmine_release_notes. If not, see <http://www.gnu.org/licenses/>.
 
-class DoEverythingForVersion131 < ActiveRecord::Migration
+class DoEverythingForVersion131 < ActiveRecord::Migration[5.2]
   def up
     # remove release_notes.status => now a custom field
     remove_column :release_notes, :status
